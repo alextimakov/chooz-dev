ed2f23fe4efd3a6dfce26bc84e6a94a3a49df79a
Alexander <alextimakoff94@gmail.com>
2023-12-04 23:43:43 +0000

    fixed psycopg2

--- Hello.py
+++ Hello.py
@@ -3,7 +3,24 @@
 import streamlit as st
 from st_aggrid import AgGrid
 
-from scripts import select_all
+select_all = '''
+  SELECT 
+    kinopoisk_id,
+    imdb_id,
+    name_ru,
+    name_en,
+    name_orig,
+    film_type,
+    film_year, 
+    ARRAY(select jsonb_array_elements(countries::jsonb) ->> 'country') as country,
+    ARRAY(select jsonb_array_elements(genres::jsonb) ->> 'genre') as genre,
+    rating_kinopoisk,
+    rating_imdb,
+    poster_url,
+    poster_url_preview
+  FROM films
+  LIMIT 500;
+'''
 
 # Initialize connection.
 conn = st.connection("postgresql", type="sql")
@@ -12,7 +29,7 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query(select_all, ttl="1m").reset_index(drop=True)
 
 option = st.selectbox(
-   "Movies of what country to show?",
+   "Movies of what country to show? (not active)",
    ("Email", "Home phone", "Mobile phone"),
    index=None,
    placeholder="Select country...",
@@ -26,5 +43,5 @@ option = st.selectbox(
 # else:
 #     data = df.loc[df['country'].str.contains(country)]
 #     st.write("### Currenty available movies", data.sort_index())
-
+st.write("### Currenty available movies")
 AgGrid(df)
