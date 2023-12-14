8ad654054837fe72d320438b6d48872b126e3209
Alexander <alextimakoff94@gmail.com>
2023-12-14 22:38:44 +0000

    switched to movies table

--- Hello.py
+++ Hello.py
@@ -6,7 +6,7 @@ from st_aggrid import AgGrid
 select_all = '''
   SELECT 
     kinopoisk_id,
-    imdb_id,
+    -- imdb_id,
     name_ru,
     name_orig,
     film_type,
@@ -16,12 +16,26 @@ select_all = '''
     rating_kinopoisk,
     rating_imdb,
     poster_url
-  FROM films
-  LIMIT 500;
+  FROM movies
+  LIMIT 100;
 '''
 
 counter = '''
-SELECT COUNT(DISTINCT kinopoisk_id) as counter FROM films;
+SELECT 
+   COUNT(DISTINCT kinopoisk_id) as counter 
+FROM movies;
+'''
+
+countries = '''
+SELECT DISTINCT 
+   ARRAY(select jsonb_array_elements(countries::jsonb) ->> 'country') as country
+FROM movies;
+'''
+
+genres = '''
+SELECT DISTINCT 
+   ARRAY(select jsonb_array_elements(genres::jsonb) ->> 'genre') as genre
+FROM movies;
 '''
 
 # Initialize connection.
@@ -30,29 +44,36 @@ conn = st.connection("postgresql", type="sql")
 # Perform query.
 df = conn.query(select_all, ttl="1m").reset_index(drop=True)
 counter = conn.query(counter, ttl="1m").reset_index(drop=True)
+countries = conn.query(countries, ttl="1m").reset_index(drop=True)
+genres = conn.query(genres, ttl="1m").reset_index(drop=True)
 
-uniq_countries = set([i for sublst in df['country'].values for i in sublst])
-option = st.selectbox(
-   "Movies of what country to show? (not active)",
+uniq_countries = list(countries['country'].unique())
+uniq_genres = list(genres['genre'].unique())
+option_c = st.selectbox(
+   "Movies of what country to show?",
    (uniq_countries),
    index=None,
    placeholder="Select country...",
 )
 
+option_g = st.selectbox(
+   "Movies of what genre to show?",
+   (uniq_genres),
+   index=None,
+   placeholder="Select genre...",
+)
 
-# if not country:
-#     st.error("Please select at least one country.")
-# else:
-#     data = df.loc[df['country'].str.contains(country)]
-#     st.write("### Currenty available movies", data.sort_index())
 st.metric(label="Total movies", value=f"{counter.loc[0, 'counter']}")
 
-st.write("### Currenty available movies")
-if option:
-   data = df.loc[df['country'].apply(lambda x: True if option in str(x) else False)]
-   if data.shape[0] > 0:
-      AgGrid(data)
-   else:
-      st.write("#### No movies for {option}") 
+st.write("#### Currenty available movies")
+data = df
+if option_c:
+   data = data.loc[data['country'].apply(lambda x: True if option_c in str(x) else False)]
+
+if option_g:
+   data = data.loc[data['genre'].apply(lambda x: True if option_g in str(x) else False)]
+
+if data.shape[0] > 0:
+   AgGrid(data)
 else:
-   AgGrid(df)
+   st.write("#### No movies for {option_c} and {option_g}")
